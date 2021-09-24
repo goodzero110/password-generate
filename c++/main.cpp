c1ccf40bfcbc058e046782d9a6ef3085ee722a10
goodzero110 <91283604+goodzero110@users.noreply.github.com>
2021-09-24 18:44:26 +0300

    main.cpp
    passwd

---
+++ c++/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+  
+{
+  setlocale(LC_ALL, "en");
+  const char s = '\n';
+  ing passwd;
+  cout << "passwrd;" << endl;
+  cin >> passwd;
+  if (passwrd==1)
+  {
+    system("pause");
+  }
+  else
+  {
+    return 0;
+  }
+}
